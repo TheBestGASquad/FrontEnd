50d32cdc54dc33be7f76a8f2c00a6e093bed7867
JLisaW <j.lisa.wang@gmail.com>
2017-06-11 13:04:24 -0400

    adding new heroku route

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://radiant-plains-78167.herokuapp.com'
+    production: 'https://mighty-temple-35862.herokuapp.com'
     // development: 'http://localhost:4741'
   }
 }
